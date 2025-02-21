820cdde27ac0b1ca5aa3b6954ff65fa6eacebce4
Igor Gonak <igor.gonak@bonprix.net>
2025-02-21 21:49:50 +0100

    fix: correct DEFAULT_STATE import path

--- src/withAddonDecorator.tsx
+++ src/withAddonDecorator.tsx
@@ -1,11 +1,7 @@
 import React, { ReactNode } from "react";
-import {
-  makeDecorator,
-  useState,
-  addons,
-  useParameter,
-} from "storybook/internal/preview-api";
-import { KEY, EVENTS, DEFAULT_STATE } from "./constants";
+import { makeDecorator, useState, addons, useParameter } from "storybook/internal/preview-api";
+import { KEY, EVENTS } from "./constants";
+import { DEFAULT_STATE } from "./defaults";
 
 export const withAddonDecorator = makeDecorator({
   name: "withAddonDecorator",
@@ -13,19 +9,8 @@ export const withAddonDecorator = makeDecorator({
   skipIfNoParametersOrOptions: false,
   wrapper: (getStory, context, { parameters }) => {
     const initialWidth = useParameter(KEY, DEFAULT_STATE).startWidth;
-    // let initialWidth: number;
-    // const unsafeStartWidth = parameters?.viewportResizer?.startWidth;
-    // if (!unsafeStartWidth || typeof unsafeStartWidth !== "number") {
-    //   initialWidth = DEFAULT_STATE.startWidth;
-    // } else {
-    //   initialWidth = unsafeStartWidth;
-    // }
     const [width, setWidth] = useState(initialWidth);
-    addons
-      .getChannel()
-      .addListener(EVENTS.UPDATE_WIDTH, (currentWidth) =>
-        setWidth(currentWidth),
-      );
+    addons.getChannel().addListener(EVENTS.UPDATE_WIDTH, (currentWidth) => setWidth(currentWidth));
     const story = getStory(context) as ReactNode;
     return (
       <div id="with-dynamic-width-decorator" style={{ width: `${width}px` }}>
